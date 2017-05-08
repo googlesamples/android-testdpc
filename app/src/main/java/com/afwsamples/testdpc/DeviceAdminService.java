a511784e5338f904fa7ab661f8385036880c442b
Tony Mak <tonymak@google.com>
2017-05-08 14:30:01 +0100

    Add DeviceAdminService support in TestDPC
    Fix: 37657758
    Change-Id: I6e3fa0930b1bcb7a39aff8afd6cc92ffe0ff4b84

---
+++ app/src/main/java/com/afwsamples/testdpc/DeviceAdminService.java
@@ -0,0 +1,46 @@
+package com.afwsamples.testdpc;
+
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+/**
+ * To allow DPC process to be persistent and foreground.
+ *
+ * @see {@link android.app.admin.DeviceAdminService}
+ */
+@RequiresApi(api = Build.VERSION_CODES.O)
+public class DeviceAdminService extends android.app.admin.DeviceAdminService {
+
+    private BroadcastReceiver mPackageChangedReceiver;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        registerPackageChangesReceiver();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterPackageChangesReceiver();
+    }
+
+    private void registerPackageChangesReceiver() {
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_PACKAGE_ADDED);
+        intentFilter.addAction(Intent.ACTION_PACKAGE_REMOVED);
+        intentFilter.addDataScheme("package");
+        mPackageChangedReceiver = new PackageMonitorReceiver();
+        getApplicationContext().registerReceiver(mPackageChangedReceiver, intentFilter);
+    }
+
+    private void unregisterPackageChangesReceiver() {
+        if (mPackageChangedReceiver != null) {
+            getApplicationContext().unregisterReceiver(mPackageChangedReceiver);
+            mPackageChangedReceiver = null;
+        }
+    }
+}
