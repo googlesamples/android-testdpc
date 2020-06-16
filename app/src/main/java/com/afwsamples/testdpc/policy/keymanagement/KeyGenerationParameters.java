d0833cd226e6172b06deac7333eb214c17406ce6
Eran Messeri <eranm@google.com>
2020-06-16 19:06:27 +0100

    Support individual attestation in TestDPC
    Enhance TestDPC for supporting individual attestation:
    * Separate section for StrongBox options, including a new checkbox for
      use of individual attestation certificate.
    * Refactor key generation parameters into their own data container class.
    * Log the generated attestation record into logcat.
    
    Bug: 158668149
    Test: Manual
    Change-Id: I707224ba91b2414be78b0017b07fb38d0b0aa8c3

---
+++ app/src/main/java/com/afwsamples/testdpc/policy/keymanagement/KeyGenerationParameters.java
@@ -0,0 +1,66 @@
+package com.afwsamples.testdpc.policy.keymanagement;
+
+public class KeyGenerationParameters {
+    public final String alias;
+    public final boolean isUserSelectable;
+    public final byte[] attestationChallenge;
+    public final int idAttestationFlags;
+    public final boolean useStrongBox;
+    public final boolean generateEcKey;
+
+    public KeyGenerationParameters(
+            String alias, boolean isUserSelectable, byte[] attestationChallenge,
+            int idAttestationFlags, boolean useStrongBox, boolean generateEcKey) {
+        this.alias = alias;
+        this.isUserSelectable = isUserSelectable;
+        this.attestationChallenge = attestationChallenge;
+        this.idAttestationFlags = idAttestationFlags;
+        this.useStrongBox = useStrongBox;
+        this.generateEcKey = generateEcKey;
+    }
+
+    public static class Builder {
+        private String mAlias;
+        private boolean mIsUserSelectable;
+        private byte[] mAttestationChallenge;
+        private int mIdAttestationFlags;
+        private boolean mUseStrongBox;
+        private boolean mGenerateEcKey;
+
+        public Builder setAlias(String alias) {
+            mAlias = alias;
+            return this;
+        }
+
+        public Builder setIsUserSelectable(boolean isUserSelectable) {
+            mIsUserSelectable = isUserSelectable;
+            return this;
+        }
+
+        public Builder setAttestationChallenge(byte[] attestationChallenge) {
+            mAttestationChallenge = attestationChallenge;
+            return this;
+        }
+
+        public Builder setIdAttestationFlags(int idAttestationFlags) {
+            mIdAttestationFlags = idAttestationFlags;
+            return this;
+        }
+
+        public Builder setUseStrongBox(boolean useStrongBox) {
+            mUseStrongBox = useStrongBox;
+            return this;
+        }
+
+        public Builder setGenerateEcKey(boolean generateEcKey) {
+            mGenerateEcKey = generateEcKey;
+            return this;
+        }
+
+        public KeyGenerationParameters build() {
+            return new KeyGenerationParameters(mAlias, mIsUserSelectable, mAttestationChallenge,
+                    mIdAttestationFlags, mUseStrongBox, mGenerateEcKey);
+        }
+
+    }
+}
