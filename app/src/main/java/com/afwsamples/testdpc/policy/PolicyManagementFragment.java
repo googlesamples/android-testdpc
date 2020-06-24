6f02e24125ec3b69b9c337577f2721e0db874224
Jonathan Scott <scottjonathan@google.com>
2020-06-24 15:53:22 +0100

    Fix broken version check for autoTime.
    Fixes: 109

--- app/src/main/java/com/afwsamples/testdpc/policy/PolicyManagementFragment.java
+++ app/src/main/java/com/afwsamples/testdpc/policy/PolicyManagementFragment.java
@@ -2533,7 +2533,10 @@ public class PolicyManagementFragment extends BaseSearchablePolicyPreferenceFrag
 
     @TargetApi(Util.R_VERSION_CODE)
     private void reloadSetAutoTimeUi() {
-        boolean isOrgOwned = Util.SDK_INT >= VERSION_CODES.R &&
+        if (Util.SDK_INT < VERSION_CODES.R) {
+            return;
+        }
+        boolean isOrgOwned = 
                 mDevicePolicyManager.isOrganizationOwnedDeviceWithManagedProfile();
 
         if (mDevicePolicyManager.isDeviceOwnerApp(mPackageName)
@@ -2546,7 +2549,10 @@ public class PolicyManagementFragment extends BaseSearchablePolicyPreferenceFrag
 
     @TargetApi(Util.R_VERSION_CODE)
     private void reloadSetAutoTimeZoneUi() {
-        boolean isOrgOwned = Util.SDK_INT >= VERSION_CODES.R &&
+        if (Util.SDK_INT < VERSION_CODES.R) {
+            return;
+        }
+        boolean isOrgOwned =
                 mDevicePolicyManager.isOrganizationOwnedDeviceWithManagedProfile();
 
         if (mDevicePolicyManager.isDeviceOwnerApp(mPackageName)
