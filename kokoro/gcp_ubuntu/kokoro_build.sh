8a85a0030a4e9a3bdb3662f070f5ed1101660fbe
Jonathan Scott <scottjonathan@google.com>
2022-08-08 10:47:19 +0000

    .

--- kokoro/gcp_ubuntu/kokoro_build.sh
+++ kokoro/gcp_ubuntu/kokoro_build.sh
@@ -4,7 +4,7 @@
 set -e
 
 sudo npm install -g @bazel/bazelisk
-/usr/lib/android-sdk/cmdline-tools/bin/sdkmanager --sdk_root=/usr/lib/android-sdk "platforms;android-33"
+$ANDROID_HOME/tools/bin/sdkmanager "platforms;android-33"
 
 cd "${KOKORO_ARTIFACTS_DIR}/github/android-testdpc"
 ./build.sh
