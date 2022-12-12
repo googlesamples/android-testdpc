0b6524310da77976af2b812d8f150ca303b7ffe7
TestDPC <scottjonathan@google.com>
2022-12-12 16:56:56 +0100

    No public description
    PiperOrigin-RevId: 494728729

--- src/main/java/com/afwsamples/testdpc/policy/SecurityLogsFragment.java
+++ src/main/java/com/afwsamples/testdpc/policy/SecurityLogsFragment.java
@@ -25,6 +25,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
+import android.util.ArrayMap;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -33,10 +34,14 @@ import com.afwsamples.testdpc.R;
 import com.afwsamples.testdpc.common.ReflectionUtil;
 import com.afwsamples.testdpc.common.ReflectionUtil.ReflectionIsTemporaryException;
 import com.afwsamples.testdpc.common.Util;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.lang.IllegalAccessException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @TargetApi(VERSION_CODES.N)
@@ -44,6 +49,8 @@ public class SecurityLogsFragment extends ListFragment {
   private static final String TAG = "ProcessLogsFragment";
 
   private static final String PRE_REBOOT_KEY = "pre-reboot";
+
+  private static Map<Integer, String> sTagNames = getTagNames();
 
   private final ArrayList<String> mLogs = new ArrayList<>();
   private ArrayAdapter<String> mAdapter;
@@ -143,112 +150,22 @@ public class SecurityLogsFragment extends ListFragment {
     }
   }
 
-  public static String getStringEventTagFromId(int eventId) {
-    final String eventTag;
-    switch (eventId) {
-      case SecurityLog.TAG_ADB_SHELL_INTERACTIVE:
-        eventTag = "ADB_SHELL_INTERACTIVE";
-        break;
-      case SecurityLog.TAG_ADB_SHELL_CMD:
-        eventTag = "ADB_SHELL_CMD";
-        break;
-      case SecurityLog.TAG_SYNC_RECV_FILE:
-        eventTag = "SYNC_RECV_FILE";
-        break;
-      case SecurityLog.TAG_SYNC_SEND_FILE:
-        eventTag = "SYNC_SEND_FILE";
-        break;
-      case SecurityLog.TAG_APP_PROCESS_START:
-        eventTag = "APP_PROCESS_START";
-        break;
-      case SecurityLog.TAG_KEYGUARD_DISMISSED:
-        eventTag = "KEYGUARD_DISMISSED";
-        break;
-      case SecurityLog.TAG_KEYGUARD_DISMISS_AUTH_ATTEMPT:
-        eventTag = "KEYGUARD_DISMISS_AUTH_ATTEMPT";
-        break;
-      case SecurityLog.TAG_KEYGUARD_SECURED:
-        eventTag = "KEYGUARD_SECURED";
-        break;
-      case SecurityLog.TAG_OS_STARTUP:
-        eventTag = "OS_STARTUP";
-        break;
-      case SecurityLog.TAG_OS_SHUTDOWN:
-        eventTag = "OS_SHUTDOWN";
-        break;
-      case SecurityLog.TAG_LOGGING_STARTED:
-        eventTag = "LOGGING_STARTED";
-        break;
-      case SecurityLog.TAG_LOGGING_STOPPED:
-        eventTag = "LOGGING_STOPPED";
-        break;
-      case SecurityLog.TAG_MEDIA_MOUNT:
-        eventTag = "MEDIA_MOUNT";
-        break;
-      case SecurityLog.TAG_MEDIA_UNMOUNT:
-        eventTag = "MEDIA_UNMOUNT";
-        break;
-      case SecurityLog.TAG_LOG_BUFFER_SIZE_CRITICAL:
-        eventTag = "LOG_BUFFER_SIZE_CRITICAL";
-        break;
-      case SecurityLog.TAG_PASSWORD_EXPIRATION_SET:
-        eventTag = "PASSWORD_EXPIRATION_SET";
-        break;
-      case SecurityLog.TAG_PASSWORD_COMPLEXITY_SET:
-        eventTag = "PASSWORD_COMPLEXITY_SET";
-        break;
-      case SecurityLog.TAG_PASSWORD_HISTORY_LENGTH_SET:
-        eventTag = "PASSWORD_HISTORY_LENGTH_SET";
-        break;
-      case SecurityLog.TAG_MAX_SCREEN_LOCK_TIMEOUT_SET:
-        eventTag = "MAX_SCREEN_LOCK_TIMEOUT_SET";
-        break;
-      case SecurityLog.TAG_MAX_PASSWORD_ATTEMPTS_SET:
-        eventTag = "MAX_PASSWORD_ATTEMPTS_SET";
-        break;
-      case SecurityLog.TAG_KEYGUARD_DISABLED_FEATURES_SET:
-        eventTag = "KEYGUARD_DISABLED_FEATURES_SET";
-        break;
-      case SecurityLog.TAG_REMOTE_LOCK:
-        eventTag = "REMOTE_LOCK";
-        break;
-      case SecurityLog.TAG_WIPE_FAILURE:
-        eventTag = "WIPE_FAILURE";
-        break;
-      case SecurityLog.TAG_KEY_GENERATED:
-        eventTag = "KEY_GENERATED";
-        break;
-      case SecurityLog.TAG_KEY_IMPORT:
-        eventTag = "KEY_IMPORT";
-        break;
-      case SecurityLog.TAG_KEY_DESTRUCTION:
-        eventTag = "KEY_DESTRUCTION";
-        break;
-      case SecurityLog.TAG_USER_RESTRICTION_ADDED:
-        eventTag = "USER_RESTRICTION_ADDED";
-        break;
-      case SecurityLog.TAG_USER_RESTRICTION_REMOVED:
-        eventTag = "USER_RESTRICTION_REMOVED";
-        break;
-      case SecurityLog.TAG_CERT_AUTHORITY_INSTALLED:
-        eventTag = "CERT_AUTHORITY_INSTALLED";
-        break;
-      case SecurityLog.TAG_CERT_AUTHORITY_REMOVED:
-        eventTag = "CERT_AUTHORITY_REMOVED";
-        break;
-      case SecurityLog.TAG_CRYPTO_SELF_TEST_COMPLETED:
-        eventTag = "CRYPTO_SELF_TEST_COMPLETED";
-        break;
-      case SecurityLog.TAG_KEY_INTEGRITY_VIOLATION:
-        eventTag = "KEY_INTEGRITY_VIOLATION";
-        break;
-      case SecurityLog.TAG_CERT_VALIDATION_FAILURE:
-        eventTag = "CERT_VALIDATION_FAILURE";
-        break;
-      default:
-        eventTag = "UNKNOWN(" + eventId + ")";
+  private static Map<Integer, String> getTagNames() {
+    Map<Integer, String> tagNames = new ArrayMap<Integer, String>();
+    for (Field f : SecurityLog.class.getDeclaredFields()) {
+      if (f.getName().startsWith("TAG_") && ((f.getModifiers() & Modifier.PUBLIC) > 0)) {
+        try {
+          tagNames.put(f.getInt(null), f.getName().substring(4));
+        } catch (IllegalAccessException e) {
+          Log.e(TAG, "Failed to read field " + f, e);
+        }
+      }
     }
-    return eventTag;
+    return tagNames;
+  }
+
+  public static String getStringEventTagFromId(int eventId) {
+    return sTagNames.getOrDefault(eventId, "UNKNOWN(" + eventId + ")");
   }
 
   public static void printData(StringBuilder sb, Object data) {
