fc06369c4de3f5fd32cc8bb2a3b3716aac00e402
Ahsan Hussain <ahsanhussain@ahsanh.onmicrosoft.com>
2026-01-22 14:18:05 -0500

    fix: use dynamic versions for setup libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -118,8 +118,8 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Setup Wizard
-    implementation("com.google.android.setupdesign:setupdesign:2.7.1")
-    implementation("com.google.android.setupcompat:setupcompat:1.1.2")
+    implementation("com.google.android.setupdesign:setupdesign:+")
+    implementation("com.google.android.setupcompat:setupcompat:+")
 
     // Google Guava (from Bazel setup)
     implementation("com.google.guava:guava:31.1-android")
